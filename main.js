e9272f9a6eb6ceef5fac0dcea66c4ee5e85edcc4
Kenny Castellano <kennycastellano@gmail.com>
2020-10-29 22:19:25 -0500

    Updated navbar

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-// Set up app, point to public dir
 var express = require('express');
+var exphbs  = require('express-handlebars');
 var app = express();
+var path = require('path');
 app.use('/static', express.static('public'));
 
 // require body parser to form data handlebars from html template
@@ -8,11 +9,13 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 var handlebars = require('express-handlebars').create(
   {
-    defaultLayout:'main'
+    defaultLayout:'main',
+    layoutsDir: path.join[__dirname, 'views/layouts/'],
+    partialsDir: path.join[__dirname, 'views/partials/']
   }
 );
 
-app.engine("handlebars", handlebars.engine);
+app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', 3023);
@@ -25,13 +28,21 @@ app.get("/", function(req, res)
   res.render('home', context);
 })
 
-app.use('/customers', require('./public/js/customers.js'));
+app.get("/userProfile", function(req, res)
+{
+  var scripts = [{script: '/public/js/userProfile.js'}]
+  res.render('userProfile', scripts);
+})
 
+app.post("/nearbyTrails", function(req, res)
+{
+  var scripts = [{script: '/public/js/nearbyTrails.js'}]
+  res.render('nearbyTrails', scripts);
+})
 
-app.use('/userProfile', require('./public/js/userProfile.js'));
+//app.use('/userProfile', require('./public/js/userProfile.js'));
 
-app.use('/nearbyTrails', require('./public/js/nearbyTrails'));
-
+//app.use('/nearbyTrails', require('./public/js/nearbyTrails'));
 
 app.use(function(req,res){
   res.status(404);
@@ -46,4 +57,4 @@ app.use(function(err, req, res, next){
 
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-  });  
+  });
