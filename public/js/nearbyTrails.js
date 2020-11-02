b6d1f397b9d1875cf2c8ed33f5a0935dd23b4c75
zhenliu7 <59631141+zhenliu7@users.noreply.github.com>
2020-11-02 18:34:25 -0500

    Nearby Trails with functions added
    Nearby trails page with trailinfo stored in context

--- public/js/nearbyTrails.js
+++ public/js/nearbyTrails.js
@@ -5,11 +5,8 @@ module.exports = function()
     var router = express.Router();
 
     let context = {};
-    context.jsscripts = ["nearbyTrailsPage.js"];
-    context.css = ["nearbyTrails.css"];
-
-    context.trails = [{trail_name:"fun time trail", trail_length:"10", distance_to_trail:"15"},
-                    {trail_name:"sad time trail", trail_length:"100", distance_to_trail:"1"}];
+    context.jsscripts = ["userProfilePage.js"];
+    context.css = ["userProfile.css"];
 
     router.get("/", function(req, res)
     {
@@ -18,56 +15,62 @@ module.exports = function()
 
     router.post("/", function(req, res)
     {
-        res.render('nearbyTrails', context);
+        geocode().then(trailInfo => {
+          //passing received trail info to context to be rendered
+          context.trailInfo = trailInfo;
+          // nearbytrails page is rendered and context is passed
+          res.render('nearbyTrails', context);
+        });
 
         function geocode(){
-            // function to receive lon/lat information using zipcode from cookies
-            var zipcode = req.cookies.zipCode;
-            return axios.get('http://www.mapquestapi.com/geocoding/v1/address',{
-              params:{
-                key:'zFqc6WEQ0wFDfMKcyhV3pJJpkrxPV5bG',
-                location:zipcode
-              }
-            })
-            .then(function(response){
-              // lat and long from response
-              var lat = response.data.results[0].locations[0].latLng.lat;
-              var lng = response.data.results[0].locations[0].latLng.lng;
-              // to convert zipcode to lon/lat
-              return trails(lat, lng);
-            })
-            .catch(function(error){
-              console.log(error);
-            });
-          }
-  
-          function trails(lat, lng){
-            //Retrieve trail info
-            return axios.get('https://www.hikingproject.com/data/get-trails',{
-              params:{
-                lat: lat,
-                lon: lng,
-                key:'200964084-38fe0d265a453ee01c59068d6f6d3ac9'
-              }
-            })
-            .then(function(response){
-              //Stores all the returned trail information
-              let trailInfo = response.data.trails.map((trail) => {
-                  return {
-                    // For selecting the required parameters
-                      name: trail.name,
-                      length: trail.length,
-                      longitude: trail.longitude,
-                      latitude: trail.latitude
-                  };
-              });  
-              return trailInfo
-            })
-            .catch(function(error){
-              console.log(error);
-            });
-          }
-    })
+          // function to receive lon/lat information using zipcode from cookies
+          var zipcode = req.cookies.userData.zipCode;
+          //submitting request to mapquest
+          return axios.get('http://www.mapquestapi.com/geocoding/v1/address',{
+            params:{
+              key:'zFqc6WEQ0wFDfMKcyhV3pJJpkrxPV5bG',
+              location:zipcode
+            }
+          })
+          .then(function(response){
+            // lat and long from response
+            var lat = response.data.results[0].locations[0].latLng.lat;
+            var lng = response.data.results[0].locations[0].latLng.lng;
+            // to convert zipcode to lon/lat
+            return trails(lat, lng);
+          })
+          .catch(function(error){
+            console.log(error);
+          });
+        }
+
+        function trails(lat, lng){
+          //Retrieve trail info from hiking project
+          return axios.get('https://www.hikingproject.com/data/get-trails',{
+            params:{
+              lat: lat,
+              lon: lng,
+              key:'200964084-38fe0d265a453ee01c59068d6f6d3ac9'
+            }
+          })
+          .then(function(response){
+            //Stores all the returned trail information
+            let trailInfo = response.data.trails.map((trail) => {
+                return {
+                  // For selecting the required parameters
+                    name: trail.name,
+                    length: trail.length,
+                    longitude: trail.longitude,
+                    latitude: trail.latitude
+                };
+            });  
+            return trailInfo
+          })
+          .catch(function(error){
+            console.log(error);
+          });
+        }
+        })
 
     return router;
 }();
