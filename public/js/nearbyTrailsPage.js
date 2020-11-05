fdf5f338bc9448f2e4e507ae307de541f5d1bbf1
Riley <rkjoinnides@gmail.com>
2020-11-05 07:46:31 -0500

    mood based trails done

--- public/js/nearbyTrailsPage.js
+++ public/js/nearbyTrailsPage.js
@@ -6,12 +6,65 @@ function openMapTab(latitude, longitude)
 
 function clearTable()
 {
-    $("#trailTable").empty();
+    $("#trailsTable").empty();
 }
 
-function addTrailsToTable(trails)
+function renderAllTrails()
 {
-    trails.forEach(element => {
-        console.log(element);
-    });
+    let tableRows = $('#trailsTable > tbody > tr');
+
+
+    for (let i = 0; i < tableRows.length; i++) 
+    {
+        const element = tableRows[i];
+        element.style.display = "";
+    }
+}
+
+function sortTrails()
+{
+    let desiredMood = getDesiredMood()
+    if (desiredMood == null)
+    {
+        renderAllTrails();
+        return;
+    }
+
+    let tableRows = $('#trailsTable > tbody > tr');
+
+
+    for (let i = 0; i < tableRows.length; i++) 
+    {
+        const element = tableRows[i];
+        // last column with relDiff is hidden
+        if (element.lastElementChild.innerHTML != desiredMood)
+        {
+            element.style.display = "none"
+        }
+        else
+        {
+            element.style.display = ""
+        }
+    }
+    
+}
+
+function getDesiredMood()
+{
+    if ($("#easyAndChill").is(":checked"))
+    {
+        return 1;
+    }
+    else if (($("#matchFitness").is(":checked")))
+    {
+        return 2;
+    }
+    else if (($("#challengeMe").is(":checked")))
+    {
+        return 3;
+    }
+    else
+    {
+        return null;
+    }
 }
