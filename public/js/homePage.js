19f2e77e0f72b43cb954d8eb4252fc862264f078
Riley <rkjoinnides@gmail.com>
2020-11-30 21:41:38 -0500

    fixed broken home page

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -1,5 +1,10 @@
 let submitButton = $("#zipSubmit");
 
+submitButton.click(function()
+{
+    Cookies.set("zipCode", $("#userZipInput").val())
+})
+
 window.onload = function() {
 	document.getElementById("navbarItems").children[0].classList.add("active")
 }
